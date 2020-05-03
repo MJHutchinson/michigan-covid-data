293e80130156127791e72d0c3e8fcff1b2468371
Michael Hutchinson <mhutchin@zizgpu01.cpu.stats.ox.ac.uk>
2020-05-03 15:00:02 +0100

    Automatic update and commit

--- poll_website.sh
+++ poll_website.sh
@@ -1,16 +1,17 @@
 cd ~/michigan-covid-data
+. ~/.bash_profile
 
 wget -O website_pull.html https://www.michigan.gov/coronavirus/0,9753,7-406-98163_98173---,00.html
 source venv/bin/activate
 
 python process_snapshot.py
 
-rm website_pull.html
+rm -f website_pull.html
 
 git add website-snapshots/*
 
-git commit -m 'Automatic update and push'
+git commit -m 'Automatic update and commit'
 
-git push
+# git push
 
 echo "Succesful website poll"
